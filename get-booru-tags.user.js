56be56ffb9a21e06a8b0345841ec1987ec8e3a83
Furqanil Taqwa <50163983+Linaqruf@users.noreply.github.com>
2024-06-20 17:55:03 +0700

    feat: add support for Animagine XL V3 prompt format

--- get-booru-tags.user.js
+++ get-booru-tags.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Get Booru Tags
+// @name         Get Booru Tags with Enhanced Tag Ordering
 // @namespace    https://github.com/onusai/
-// @version      0.4.8
-// @description  Press the [~] tilde key under ESC to open a prompt with all tags
+// @version      0.5.1
+// @description  Press the [~] tilde key under ESC to open a prompt with all tags including improved tag ordering
 // @author       Onusai#6441
 // @match        https://gelbooru.com/index.php?page=post&s=view*
 // @match        https://safebooru.donmai.us/posts/*
@@ -12,32 +12,53 @@
 // @license MIT
 // ==/UserScript==
 
-// todo: add support for safebooru.org/index.php?page=post&s=view* and realbooru.com
-
 (function() {
     'use strict';
 
-    // edit to change default behavior
-    let include_commas = true;          // seperate each tag with a comma
-    let remove_underscores = true;      // replace underscores with spaces
-    let remove_parentheses = false;     // remove parentheses from tags
-    let randomize_tag_order = false;    // randomizes tags within each group, group order stays the same
-    let escape_colons = false;          // escapes colons, usually has no impact
+    let include_commas = true;
+    let remove_underscores = true;
+    let remove_parentheses = false;
+    let randomize_tag_order = false;
+    let escape_colons = false;
 
-    // edit to change tag group order or remove certain groups completely
-    let tag_group_order = ["character", "general", "artist", "copyright"]; // "metadata"
+    // Updated tag group order with "special_tags" as a custom category for enhanced ordering
+    // let tag_group_order = ["special_tags", "character", "copyright", "artist", "general"];
+    let tag_group_order = ["special_tags", "character", "copyright", "general"];
 
-    // edit to change hotkeys
     let hotkey_default = '`';
-    let hotkey_1 = '1'; // randomize tags
+    let hotkey_1 = '1';
 
+    let special_tags = [
+        "1girl", "2girls", "3girls", "4girls", "5girls", "6+girls", "multiple girls",
+        "1boy", "2boys", "3boys", "4boys", "5boys", "6+boys", "multiple boys", "male focus",
+        "1other", "2others", "3others", "4others", "5others", "6+others", "multiple others", "other focus",
+    ];
 
-    function randomize_tags(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
+    let blacklist_tags = [
+        ".*background",
+        ".*text.*",
+        ".*blurry.*",
+        "comic",
+        ".*manga.*",
+        ".*magazine.*",
+        ".*username.*",
+        "artist name",
+        "lowres",
+        "bad anatomy",
+        "bad hands",
+        "text",
+        "error",
+        "missing fingers",
+        "extra digit",
+        "fewer digits",
+        "cropped",
+        "jpeg artifacts",
+        "signature",
+        "watermark",
+        "username",
+        "blurry",
+        "artist name"
+    ];
 
     let keysPressed = {};
 
@@ -49,17 +70,22 @@
         keysPressed[event.key] = true;
         if (!keysPressed[hotkey_default]) return;
         if (event.key == hotkey_1) show_prompt(true);
-    })
-
+    });
 
     function show_prompt(randomize=false) {
         for (var member in keysPressed) delete keysPressed[member];
 
         let tags = null;
         let url = window.location.href;
-        if (url.includes("/gelbooru.com"))tags = get_gel_tags(randomize);
+        if (url.includes("/gelbooru.com")) tags = get_gel_tags(randomize);
         else if (url.includes("/danbooru.donmai.us") || url.includes("/safebooru.donmai.us") || url.includes("/aibooru.online")) tags = get_dan_tags(randomize);
         if (!tags) return;
+
+        // Filter out blacklisted tags using regex
+        tags = tags.filter(tag => !blacklist_tags.some(pattern => new RegExp(pattern).test(tag)));
+
+        // New: Reordering based on special_tags criteria before displaying
+        tags = reorder_tags_based_on_special_criteria(tags);
 
         let tag_count = tags.length;
 
@@ -77,6 +103,17 @@
         prompt("Prompt: " + tag_count + " tags", tags);
     }
 
+    function reorder_tags_based_on_special_criteria(tags) {
+        // Separating general tags and filtering special tags to the front
+        let generalTags = tags.filter(tag => tag_group_order.includes("general") && special_tags.includes(tag));
+        let otherTags = tags.filter(tag => !special_tags.includes(tag));
+
+        // Ordering general tags based on special_tags order
+        let orderedGeneralTags = special_tags.filter(tag => generalTags.includes(tag));
+
+        // Combine the ordered general tags with the rest of the tags
+        return [...orderedGeneralTags, ...otherTags];
+    }
 
     function get_gel_tags(randomize=false) {
         let tags = [];
@@ -88,7 +125,6 @@
         }
         return tags;
     }
-
 
     function get_dan_tags(randomize=false) {
         let tags = [];
